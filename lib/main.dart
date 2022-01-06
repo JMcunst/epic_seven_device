293b0b52b9c21a627d987fe9e4a37c7099d27ee3
Tyler <tyler.hwang@ebridge-world.com>
2022-01-06 12:27:45 +0900

    update : netflix ui first

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,61 @@
+import 'package:epic_seven_device/widget/bottom_bar.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-import 'login_page.dart';
+import 'screen/login_page.dart';
 
 void main() {
   runApp(new MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  TabController? controller;
+
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'login demo',
-      theme: new ThemeData(
-        primarySwatch: Colors.blue,
+    return MaterialApp(
+      title: 'ESDevice',
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: Colors.black,
+        colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.blue, brightness: Brightness.dark)
+            .copyWith(secondary: Colors.black),
       ),
-      home: new LoginPage(),
+      home: DefaultTabController(
+        length: 4,
+        child: Scaffold(
+          body: TabBarView(
+            physics: const NeverScrollableScrollPhysics(),
+            children: [
+              Container(
+                child: const Center(
+                  child: Text('home'),
+                ),
+              ),
+              Container(
+                child: const Center(
+                  child: Text('search'),
+                ),
+              ),
+              Container(
+                child: const Center(
+                  child: Text('save'),
+                ),
+              ),
+              Container(
+                child: const Center(
+                  child: Text('more'),
+                ),
+              ),
+            ],
+          ),
+          bottomNavigationBar: Bottom(),
+        ),
+      ),
     );
   }
 }
