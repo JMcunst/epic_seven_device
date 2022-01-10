0153ecbdebb92d9ae40188da19d4b6e40dfc2cb6
Tyler <tyler.hwang@ebridge-world.com>
2022-01-10 16:34:58 +0900

    update: detail page

--- lib/screen/detail_screen.dart
+++ lib/screen/detail_screen.dart
@@ -12,10 +12,12 @@ class DetailScreen extends StatefulWidget {
 }
 
 class _DetailScreenState extends State<DetailScreen> {
+  bool like = false;
+
   @override
   void initState() {
     super.initState();
-    var like = widget.movie?.like;
+    like = widget.movie!.like;
   }
 
   @override
@@ -46,11 +48,60 @@ class _DetailScreenState extends State<DetailScreen> {
                             child: Column(
                               children: <Widget>[
                                 Container(
-                                  padding: EdgeInsets.fromLTRB(0, 45, 0, 10),
+                                  padding:
+                                      const EdgeInsets.fromLTRB(0, 45, 0, 10),
                                   child: Image.asset(
                                       'images/' + widget.movie!.poster),
                                   height: 300,
-                                )
+                                ),
+                                Container(
+                                  padding: const EdgeInsets.all(7),
+                                  child: const Text(
+                                    '99% dlfcl 2019 15+ 시즌 1개',
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(fontSize: 13),
+                                  ),
+                                ),
+                                Container(
+                                  padding: const EdgeInsets.all(7),
+                                  child: Text(
+                                    widget.movie!.title,
+                                    textAlign: TextAlign.center,
+                                    style: const TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 16),
+                                  ),
+                                ),
+                                Container(
+                                  padding: const EdgeInsets.all(3),
+                                  child: TextButton(
+                                    onPressed: () {},
+                                    style: TextButton.styleFrom(
+                                      backgroundColor: Colors.red,
+                                    ),
+                                    child: Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      children: const <Widget>[
+                                        Icon(Icons.play_arrow),
+                                        Text('재생'),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                                Container(
+                                  padding: const EdgeInsets.all(5),
+                                  child: Text(widget.movie!.toString()),
+                                ),
+                                Container(
+                                  padding: const EdgeInsets.all(5),
+                                  alignment: Alignment.centerLeft,
+                                  child: const Text(
+                                    '출연: 현빈, 손예진 \n제작자: 이정효, 박지은',
+                                    style: TextStyle(
+                                        color: Colors.white60, fontSize: 12),
+                                  ),
+                                ),
                               ],
                             ),
                           ),
@@ -58,19 +109,72 @@ class _DetailScreenState extends State<DetailScreen> {
                       ),
                     ),
                   ),
+                  Positioned(
+                      child: AppBar(
+                    backgroundColor: Colors.transparent,
+                    elevation: 0,
+                  )),
                 ],
               ),
-              makeMenuButton(),
+              Container(
+                color: Colors.black26,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                    Container(
+                      padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
+                      child: InkWell(
+                        onTap: () {},
+                        child: Column(
+                          children: <Widget>[
+                            like ? Icon(Icons.check) : Icon(Icons.add),
+                            Padding(padding: EdgeInsets.all(5)),
+                            const Text(
+                              '내가 찜한 컨텐츠',
+                              style: TextStyle(
+                                  fontSize: 11, color: Colors.white60),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+                      child: Container(
+                        child: Column(
+                          children: const <Widget>[
+                            Icon(Icons.thumb_up),
+                            Padding(padding: EdgeInsets.all(5)),
+                            Text(
+                              '평가',
+                              style: TextStyle(fontSize: 11, color: Colors.white60),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+                      child: Container(
+                        child: Column(
+                          children: const <Widget>[
+                            Icon(Icons.send),
+                            Padding(padding: EdgeInsets.all(5)),
+                            Text(
+                              '공유',
+                              style: TextStyle(fontSize: 11, color: Colors.white60),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ],
           ),
         ),
       ),
     );
   }
-}
-
-Widget makeMenuButton() {
-  return Container(
-    child: const Text('아래정보'),
-  );
 }
